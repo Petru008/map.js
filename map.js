0d6a97a0af7a64802be2636e34a32dbcef948d02
Petru Banceanu <banceanu.petru@yahoo.com>
2023-12-08 19:24:02 +0000

    Map method and exercises

--- map.js
+++ map.js
@@ -6,18 +6,34 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let  results = [];
+for(let num of nums){
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
 
+const multByTwo = function(num){
+  return num * 2; 
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
+
+const simplified = nums.map(function(num){return num * 2});
+console.log(simplified);
 
 
 // Simplfied w/ map() + arrow function
 
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
 
 // With objects:
-const students = [
+const students1 = [
   {
     id: 1,
     name: 'Mark',
@@ -37,3 +53,41 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithId = students1.map( student => [student.name, student.id]);
+console.log(studentsWithId);
+
+
+// EXERCISSES 
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, maths: 77, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+let [john, ...rest] = students.map( student => [student.name, student.results]);
+console.log(john);
+console.log(rest)
+
